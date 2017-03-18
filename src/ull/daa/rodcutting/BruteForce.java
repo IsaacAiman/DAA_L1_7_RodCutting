7f426c5e97413c6077d69a3f826aea34772d9dcd
alu0100890839 <alu0100890839@ull.edu.es>
2017-03-18 16:27:03 +0000

    Clock added as well as some improvements

--- src/ull/daa/rodcutting/BruteForce.java
+++ src/ull/daa/rodcutting/BruteForce.java
@@ -25,7 +25,7 @@ public class BruteForce {
 		}
 		int max = Integer.MIN_VALUE;
 		for(int i = 1; i <= size; i++) {
-			int aux = rod.getCoste(i) + cutRod(size - i);
+			int aux = getRod().getCoste(i) + cutRod(size - i);
 			if(max < aux) {
 				max = aux;
 			}
@@ -35,11 +35,30 @@ public class BruteForce {
 	
 	public BruteForce(int size, int coste) {
 		rod = new Rod(size, coste);
-		System.out.println(rod);
+		//System.out.println(rod);
 	}
 	
 	public static void main(String args[]) {
-		BruteForce b = new BruteForce(5, 10);
-		System.out.println(b.cutRod(5));
+		Clock c = new Clock();
+		for(int i = 20; i < 35; i++) {
+			c.start();
+			BruteForce b = new BruteForce(i, i * 3);
+			b.cutRod(i);
+			System.out.println("Tiempo transcurrido para n=" + i + ": " + c.stop() + "ms\n");
+		}
+	}
+
+	/**
+	 * @return the rod
+	 */
+	public Rod getRod() {
+		return rod;
+	}
+
+	/**
+	 * @param rod the rod to set
+	 */
+	public void setRod(Rod rod) {
+		this.rod = rod;
 	}
 }
